8702b73616505933ff4a43713c57ec6ac3e6450d
doli82 <39415174+doli82@users.noreply.github.com>
2020-08-08 14:07:24 -0300

    Construção do componente Landing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import './assets/styles/global.css'
+import Landing from './pages/Landing';
 
 function App() {
   return (
-    <div className="App">APP</div>
+    <Landing></Landing>
   );
 }
 
