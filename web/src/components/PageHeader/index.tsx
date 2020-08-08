b507735b2db6e0c3b96541285926bf7b4d94c607
doli82 <39415174+doli82@users.noreply.github.com>
2020-08-08 19:03:30 -0300

    Construção do componente PageHeader

---
+++ web/src/components/PageHeader/index.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import logoImg from '../../assets/images/logo.svg'
+import backImg from '../../assets/images/icons/back.svg'
+
+import './styles.css'
+
+interface PageHeaderProps {
+    title: string
+}
+
+const PageHeader: React.FC<PageHeaderProps> = (props) => (
+  <header className="page-header">
+    <div className="top-bar-container">
+      <Link to="/">
+        <img src={backImg} />
+      </Link>
+      <img src={logoImg} alt="Proffy" />
+    </div>
+
+    <div className="header-content">
+      <strong>{props.title}</strong>
+      {props.children}
+    </div>
+  </header>
+)
+export default PageHeader
