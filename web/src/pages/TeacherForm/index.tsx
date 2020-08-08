18e6ff6e11c526c78a9f41627b99a8b9a5a174cb
doli82 <39415174+doli82@users.noreply.github.com>
2020-08-08 19:08:34 -0300

    Iniciando estruturação da página TeacherForm

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../../components/PageHeader'
+
+import './styles.css'
+
+const TeacherForm = () => (
+  <div id="page-teacher-form">
+    <PageHeader title="Que incrível que você quer dar aulas." />
+  </div>
+)
+
+export default TeacherForm
