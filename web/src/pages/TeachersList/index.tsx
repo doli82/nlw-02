1f7de4b4545fc2f28cd7ce886825eebc53d85576
doli82 <39415174+doli82@users.noreply.github.com>
2020-08-09 03:20:03 -0300

    Implementação da pesquisa de professores em TeacherList

--- web/src/pages/TeachersList/index.tsx
+++ web/src/pages/TeachersList/index.tsx
@@ -1,46 +1,76 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 
 import './styles.css'
 import PageHeader from '../../components/PageHeader'
 
-import TeacherItem from '../../components/TeacherItem'
+import TeacherItem, { Teacher } from '../../components/TeacherItem'
 import Input from '../../components/Input'
 import Select from '../../components/Select'
+import api from '../../services/api'
 
-const TeachersList = (): JSX.Element => (
-  <div id="page-teacher-list">
-    <PageHeader title="Estes são os proffys disponíveis.">
-      <form id="search-teachers">
-        <Select 
-          label="Matéria" 
-          name="subject"
-          options={[
-            {value: "Eletromagnetismo", label: "Eletromagnetismo"},
-            {value: "Eletrônica Aplicada", label: "Eletrônica Aplicada"},
-            {value: "Inteligência Artificial", label: "Inteligência Artificial"}
-          ]}
-        ></Select>
-        <Select 
-          label="Dia da semana" 
-          name="week_day"
-          options={[
-            {value: "0", label: "Domingo"},
-            {value: "1", label: "Segunda-feira"},
-            {value: "2", label: "Terça-feira"},
-            {value: "3", label: "Quarta-feira"},
-            {value: "4", label: "Quinta-feira"},
-            {value: "5", label: "Sexta-feira"},
-            {value: "6", label: "Sábado"},
-          ]}
-        ></Select>
-        <Input label="Hora" name="time" type="time"></Input>
-      </form>
-    </PageHeader>
+const TeachersList = (): JSX.Element => {
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+  const [teachers, setTeachers] = useState([]);
 
-    <main>
-      <TeacherItem />
-    </main>
-  </div>
-)
+  async function searchTeachers(event: FormEvent){
+    event.preventDefault()
+    const response = await api.get('classes', {
+      params:{
+        subject,
+        week_day,
+        time
+      }
+    })
+    setTeachers(response.data)
+  }
 
+  return (
+    <div id="page-teacher-list">
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form id="search-teachers" onSubmit={searchTeachers}>
+          <Select 
+            label="Matéria" 
+            name="subject"
+            options={[
+              {value: "Eletromagnetismo", label: "Eletromagnetismo"},
+              {value: "Eletrônica Aplicada", label: "Eletrônica Aplicada"},
+              {value: "Inteligência Artificial", label: "Inteligência Artificial"}
+            ]}
+            value={subject}
+            onChange={(event)=>setSubject(event.target.value)}
+          ></Select>
+          <Select 
+            label="Dia da semana" 
+            name="week_day"
+            options={[
+              {value: "0", label: "Domingo"},
+              {value: "1", label: "Segunda-feira"},
+              {value: "2", label: "Terça-feira"},
+              {value: "3", label: "Quarta-feira"},
+              {value: "4", label: "Quinta-feira"},
+              {value: "5", label: "Sexta-feira"},
+              {value: "6", label: "Sábado"},
+            ]}
+            value={week_day}
+            onChange={(event)=>setWeekDay(event.target.value)}
+          ></Select>
+          <Input 
+          label="Hora" 
+          name="time" 
+          type="time"          
+          value={time}
+          onChange={(event)=>setTime(event.target.value)}
+          ></Input>
+          <button type='submit'>Buscar</button>
+        </form>
+      </PageHeader>
+
+      <main>
+        {teachers.map((teacher: Teacher)=>(<TeacherItem key={teacher.id} teacher={teacher} />))}
+      </main>
+    </div>
+  )
+        }
 export default TeachersList
