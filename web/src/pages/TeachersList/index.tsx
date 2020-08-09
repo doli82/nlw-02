63cb616f99932f480935616d4d7fb3ad16597e86
doli82 <39415174+doli82@users.noreply.github.com>
2020-08-09 00:20:58 -0300

    Adição do componente input ao frontent

--- web/src/pages/TeachersList/index.tsx
+++ web/src/pages/TeachersList/index.tsx
@@ -4,25 +4,15 @@ import './styles.css'
 import PageHeader from '../../components/PageHeader'
 
 import TeacherItem from '../../components/TeacherItem'
+import Input from '../../components/Input'
 
 const TeachersList = (): JSX.Element => (
   <div id="page-teacher-list">
     <PageHeader title="Estes são os proffys disponíveis.">
       <form id="search-teachers">
-        <div className="input-block">
-          <label htmlFor="subject">Matéria</label>
-          <input type="text" id="subject" />
-        </div>
-
-        <div className="input-block">
-          <label htmlFor="week_day">Dia da semana</label>
-          <input type="text" id="week_day" />
-        </div>
-
-        <div className="input-block">
-          <label htmlFor="time">Hora</label>
-          <input type="text" id="time" />
-        </div>
+        <Input label="Matéria" name="subject"></Input>
+        <Input label="Dia da semana" name="week_day"></Input>
+        <Input label="Hora" name="time" type="time"></Input>
       </form>
     </PageHeader>
 
