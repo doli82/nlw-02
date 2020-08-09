1243e991223537fde5f6ee0fef75a573ca5775ba
doli82 <39415174+doli82@users.noreply.github.com>
2020-08-09 03:20:58 -0300

    Implementação da ação de contato e criação de nova conexão

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,51 @@ import React from 'react'
 import iconWhatsapp from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api'
 
-const TeacherItem = () => (
-  <article className="teacher-item">
-    <header>
-      <img src="https://avatars2.githubusercontent.com/u/39415174?s=460&u=f8435de293bde7eb6fd3d2509ab82bb87a322601&v=4" />
-      <div>
-        <strong>Daniel Oliveira</strong>
-        <span>Matéria</span>
-      </div>
-    </header>
-    <p>
-          Entusiasta das melhores tecnologias.
-      <br />
-      <br />
-          Descrição
-    </p>
-    <footer>
-      <p>
-            Preço/hora
-        <strong>R$80,00</strong>
-      </p>
-      <button type="button">
-        <img src={iconWhatsapp} alt=""/>
-            Entrar em contato
-      </button>
-    </footer>
-  </article>
-)
+export interface Teacher {
+  avatar: string
+  bio: string
+  cost: number
+  id: number
+  name: string
+  subject: string
+  user_id: number
+  whatsapp: string
+}
 
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.user_id
+    })
+  }
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src={teacher.avatar} alt={teacher.name}/>
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
+      <p>{teacher.bio}</p>
+      <footer>
+        <p>
+              Preço/hora
+          <strong>R$ {teacher.cost}</strong>
+        </p>
+        <a href={`https://wa.me/${teacher.whatsapp}`} onClick={createNewConnection}>
+          <img src={iconWhatsapp} alt=""/>
+              Entrar em contato
+        </a>
+      </footer>
+    </article>
+  )
+}
 export default TeacherItem
